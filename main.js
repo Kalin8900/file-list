6964a96a46dd45a6156f73b1f694e4a3899c8b5e
Michał Kalinowski <michal>
2022-10-21 22:09:59 +0200

    init

---
+++ main.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const fs = require('fs');
+require('dotenv').config();
+
+const checkDir = (path, omitFileTypes) => {
+  const files = fs.readdirSync(path).filter((file) => !omitFileTypes.includes(file.split('.').pop()));
+
+  return {
+    files,
+    count: files.length,
+    path
+  };
+};
+
+const main = async () => {
+  const app = express();
+  const port = process.env.PORT ?? 8080;
+
+  const checkDirPath = process.env.CHECK_DIR_PATH;
+
+  console.log(checkDirPath);
+
+  if (!checkDirPath) throw new Error('No directory provided');
+
+  const omitFileTypes = process.argv[2].split(',');
+
+  app.get('/', (req, res) => {
+    const dir = checkDir(checkDirPath, omitFileTypes);
+
+    res.send(dir);
+  });
+
+  app.listen(port, () => {
+    console.log(`File-list app listening at http://localhost:${port}`);
+  });
+};
+
+main();
